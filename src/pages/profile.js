062314f8735eb8029748d18f0a0f4d4bf1402b0a
ovandr <24862781+ovandr@users.noreply.github.com>
2019-01-14 21:47:07 +0300

    Capabiliy to delete user profile

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Auth } from 'aws-amplify';
+import { Button } from '@material-ui/core';
 
 export class ProfilePage extends Component {
     state = {
@@ -17,11 +18,17 @@ export class ProfilePage extends Component {
         });
     }
 
+    async deleteUser() {
+        const user = await Auth.currentAuthenticatedUser();
+        user.deleteUser(() => { });
+    }
+
     render() {
         console.log(this.state.userInfo);
         return (
             <div>
                 {this.state.userInfo ? (<pre>{JSON.stringify(this.state.userInfo, null, 2)}</pre>) : 'Loading'}
+                <Button variant="contained" color="secondary" onClick={this.deleteUser}>Delete Profile</Button>
             </div>
         )
     }
