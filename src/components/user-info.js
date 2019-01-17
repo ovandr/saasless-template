158eeaf6bcd36e64861aa4fbf901750775790cac
ovandr <24862781+ovandr@users.noreply.github.com>
2019-01-17 22:47:03 +0300

    Improve profile page

---
+++ src/components/user-info.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class UserInfo extends Component {
+  render() {
+    const { user } = this.props;
+    return (
+      <div>
+        <ul>
+          <li>Email address: {user.attributes.email}</li>
+          <li>Email verified: {user.attributes.email_verified}</li>
+        </ul>
+        <pre>{JSON.stringify(this.props.user, null, 2)}</pre>
+      </div>
+    )
+  }
+}
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { Auth } from 'aws-amplify';
+
 import { Button } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+
+import UserInfo from '../components/user-info';
 
 export class ProfilePage extends Component {
     state = {
@@ -26,10 +33,17 @@ export class ProfilePage extends Component {
     render() {
         console.log(this.state.userInfo);
         return (
-            <div>
-                {this.state.userInfo ? (<pre>{JSON.stringify(this.state.userInfo, null, 2)}</pre>) : 'Loading'}
-                <Button variant="contained" color="secondary" onClick={this.deleteUser}>Delete Profile</Button>
-            </div>
+            <Card>
+                <CardHeader title="Profile Info" />
+                <CardContent>
+                    {this.state.userInfo ? <UserInfo user={this.state.userInfo} /> : 'Loading'}
+                </CardContent>
+                <CardActions>
+                    <Button variant="contained" color="secondary" onClick={this.deleteUser}>
+                        Delete Profile
+                    </Button>
+                </CardActions>
+            </Card>
         )
     }
 }
