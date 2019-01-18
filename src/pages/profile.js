472beef209a5ad6633ce23c8e4e4786e43e81b08
ovandr <24862781+ovandr@users.noreply.github.com>
2019-01-18 20:36:51 +0300

    Confirm account deletion

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -6,45 +6,89 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 import UserInfo from '../components/user-info';
 
 export class ProfilePage extends Component {
-    state = {
-        userInfo: undefined
-    }
+  state = {
+    user: undefined,
+    showConfirmDialog: false
+  }
 
-    componentDidMount() {
-        this.updateUserInfo();
-    }
+  componentDidMount() {
+    this.updateUserInfo();
+  }
 
-    async updateUserInfo() {
-        const user = await Auth.currentUserInfo();
-        this.setState({
-            userInfo: user
-        });
-    }
+  handleClose = () => {
+    this.setState({ showConfirmDialog: false });
+  };
 
-    async deleteUser() {
-        const user = await Auth.currentAuthenticatedUser();
-        user.deleteUser(() => { });
-    }
+  async updateUserInfo() {
+    const user = await Auth.currentUserInfo();
+    this.setState({ user });
+  }
 
-    render() {
-        return (
-            <Card>
-                <CardHeader title="Profile Info" />
-                <CardContent>
-                    {this.state.userInfo ? <UserInfo user={this.state.userInfo} /> : 'Loading'}
-                </CardContent>
-                <CardActions>
-                    <Button variant="contained" color="secondary" onClick={this.deleteUser}>
-                        Delete Profile
-                    </Button>
-                </CardActions>
-            </Card>
-        )
-    }
+  deleteUser = () => {
+    this.setState({ showConfirmDialog: true });
+  }
+
+  deleteProfile = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+    user.deleteUser(() => { });
+    this.handleClose();
+  }
+
+  render() {
+    const loading = (
+      <div style={{ textAlign: 'center' }}>
+        <CircularProgress />
+      </div>
+    );
+
+    return (
+      <React.Fragment>
+        <Card>
+          <CardHeader title="Profile Info" />
+          <CardContent>
+            {this.state.user ? <UserInfo user={this.state.user} /> : loading}
+          </CardContent>
+          <CardActions>
+            <Button variant="contained" color="secondary" onClick={this.deleteUser}>
+              Delete Profile
+            </Button>
+          </CardActions>
+        </Card>
+
+        <Dialog
+          open={this.state.showConfirmDialog}
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Do you want to Delete your account?"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              All content will be deleted.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.deleteProfile} color="secondary">
+              Delete
+            </Button>
+            <Button onClick={this.handleClose} color="primary" autoFocus>
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </React.Fragment>
+    )
+  }
 }
 
 export default ProfilePage
