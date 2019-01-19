8f8e4d1c2f167597e794bfb54f437acc124e8deb
ovandr <24862781+ovandr@users.noreply.github.com>
2019-01-19 13:33:30 +0300

    Fix indent for themes files

--- src/themes/app-theme.ts
+++ src/themes/app-theme.ts
@@ -2,15 +2,15 @@ import blue from '@material-ui/core/colors/blue';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 export default createMuiTheme({
-    palette: {
-        type: 'light',
-        primary: blue
-    },
-    typography: {
-        fontSize: 14,
-        useNextVariants: true,
-    },
-    spacing: {
-        unit: 6
-    }
+  palette: {
+    type: 'light',
+    primary: blue
+  },
+  typography: {
+    fontSize: 14,
+    useNextVariants: true,
+  },
+  spacing: {
+    unit: 6
+  }
 });
--- src/themes/navigation-theme.ts
+++ src/themes/navigation-theme.ts
@@ -1,17 +1,17 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 export default createMuiTheme({
-    palette: {
-        type: 'dark',
-        text: {
-            primary: '#b7b9bc',
-        },
-        background: {
-            paper: '#19212b',
-            default: '#19212b',
-        },
+  palette: {
+    type: 'dark',
+    text: {
+      primary: '#b7b9bc',
     },
-    typography: {
-        useNextVariants: true,
+    background: {
+      paper: '#19212b',
+      default: '#19212b',
     },
+  },
+  typography: {
+    useNextVariants: true,
+  },
 });
